6f93cfc917f753c481430f089d4d821d9a0a3b06
Alberto Rico Garcia <albrigar93@gmail.com>
2017-09-07 21:48:13 +0200

    AngularClient generated

--- groceryApp/server/boot/root.js
+++ groceryApp/server/boot/root.js
@@ -1,8 +1,14 @@
 'use strict';
 
-module.exports = function(server) {
+module.exports = function (server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
+
+  router.get('/ping', function (req, res) {
+    res.send('pong');
+  });
+
   server.use(router);
+
 };
