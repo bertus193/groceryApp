8470fba7cfad7a5a85abc7de7760ae11c11a75f1
Alberto Rico Garcia <albrigar93@gmail.com>
2017-09-08 22:30:54 +0200

    SDK client configuración, prueba de funcionamiento

--- groceryApp/client/src/app/app.component.ts
+++ groceryApp/client/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { LoopBackConfig } from './sdk';
+import { Items } from './sdk/models';
+import { ItemsApi } from './sdk/services';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [ItemsApi],
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  constructor(private items: ItemsApi) {
+    LoopBackConfig.setBaseURL("127.0.0.1:3000");
+    LoopBackConfig.setApiVersion("api");
+  }
+
+  private getItems(): String {
+    let itemCount;
+
+    this.items.count().subscribe(data => {
+      itemCount = data;
+      console.log(itemCount);
+    });
+    return "salida" + this.items.count();
+  }
+
+  title = this.getItems();
 }
